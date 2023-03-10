48104e835a220d27db454bf0a113e894bbefeef7
mimimiku778 <0203.sub@gmail.com>
2023-03-10 20:48:02 +0900

    modified:   controllers/pages/IndexPageController.php 	modified:   views/header.php

--- controllers/pages/IndexPageController.php
+++ controllers/pages/IndexPageController.php
@@ -4,7 +4,7 @@ class IndexPageController extends AbstractPageController
 {
     public function index()
     {
-        View::render('header');
+        View::render('header', ['title' => 'ふわふわポートフォリオ😵']);
         View::render('toppage');
         View::render('footer');
     }
--- views/header.php
+++ views/header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
-    <title>ふわふわポートフォリオ😵</title>
+    <title><?php echo $title ?></title>
     <meta name="description" content="わたしの個人的なWEB開発のポートフォリオとソースコードを公開しているので、ぜひチェックしてみてくださいね！メインはJavaScriptとPHPでがんばっています♪ 細かいところまでこだわっているので、ぜひ見てみてください！" />
     <link rel="stylesheet" href="/../assets/mvp.css">
     <link rel="icon" type="image/png" href="/../assets/favicon.png">
