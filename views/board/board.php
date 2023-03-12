daf29162e8be833bbd1be7dcddd0276006091709
mimimiku778 <0203.sub@gmail.com>
2023-03-13 07:49:12 +0900

    modified:   views/board/board.php

--- views/board/board.php
+++ views/board/board.php
@@ -87,14 +87,12 @@
 <script>
     ((el) => {
         if (!el) return
-
+        
         const input = qS('input', el)
         const submit = qS('[type="submit"]', el)
 
         // 入力に応じてボタンの disabled を切り替え
-        input.addEventListener('input', () => {
-            submit.disabled = !validateStringNotEmpty(input.value)
-        })
+        input.addEventListener('input', () => submit.disabled = !validateStringNotEmpty(input.value))
 
         // 一部環境でボタンの disabled が効かないので、追加の処理を入れる
         el.addEventListener('submit', e => submit.disabled && e.preventDefault())
